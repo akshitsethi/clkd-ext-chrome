e276d4bed12ed19118b2dc6dda8ef1ba55f9bb6f
Akshit Sethi <akshitsethi@gmail.com>
2025-09-05 10:38:22 +0530

    Starting to work on limits based on user plan and restricting UX

---
+++ assets/js/limits.js
@@ -0,0 +1,14 @@
+// limits.js
+import { Selectors } from "./selectors.js";
+
+export const Limits = {
+    updateDOM: function() {
+
+    },
+    upgradeModal: function() {
+        
+    },
+    init: function() {
+
+    }
+};
