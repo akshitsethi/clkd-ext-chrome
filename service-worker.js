3fe8cbc428aa2127f1cdc0134ae0f3f08dee0f13
Akshit Sethi <akshitsethi@gmail.com>
2025-09-26 00:12:13 +0530

    Remove onboarding step for now

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,18 @@
 // service-worker.js
-// On-boarding page
-chrome.runtime.onInstalled.addListener(({reason}) => {
-	if (reason === 'install') {
-			chrome.tabs.create({
-			url: "onboarding.html"
-		});
-	}
-});
 
+// For future use
+// @todo add an on-boarding page that help users get a better understanding of extension features.
+//
+// On-boarding page
+// chrome.runtime.onInstalled.addListener(({reason}) => {
+// 	if (reason === 'install') {
+// 			chrome.tabs.create({
+// 			url: "onboarding.html"
+// 		});
+// 	}
+// });
+
+// Open side panel on clicking extension icon
 chrome.sidePanel
 	.setPanelBehavior({ openPanelOnActionClick: true })
 	.catch((error) => console.error(error));
